0d8fceff44fb493733bff5d23d7de32147eeab0f
Aliaksei Dziubanau <dubell1986@gmail.com>
2020-02-19 21:02:36 +0300

    fix: another fix

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 
 exports.min = function min (array) {
-  if (array.length === 0 || array === undefined){
+  if (!array || array === undefined){
     return 0;
   }
   return array.sort((a,b) => a - b)[0];
 }
 
 exports.max = function max (array) {
-  if (array.length === 0 || array === undefined){
+  if (!array || array === undefined){
     return 0;
   }
   return array.sort((a,b) => a - b)[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  if (array.length === 0 || array === undefined){
+  if (!array || array === undefined){
     return 0;
   }
   let count = 0;
