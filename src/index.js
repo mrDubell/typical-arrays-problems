0a7346034dfc32fa08a182d71e7c7490caf710bf
Aliaksei Dziubanau <dubell1986@gmail.com>
2020-02-19 21:17:03 +0300

    fix: try another

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 exports.min = function min (array) {
-  if (!array || array.length === 0){
+  if (array === undefined || array.length === 0){
     return 0;
   }
   return array.sort((a,b) => a - b)[0];
 }
 
 exports.max = function max (array) {
-  if (!array || array.length === 0){
+  if (array === undefined || array.length === 0){
     return 0;
   }
   return array.sort((a,b) => a - b)[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  if (!array || array.length === 0){
+  if (array === undefined || array.length === 0){
     return 0;
   }
   let count = 0;
